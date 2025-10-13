63b5eaa06988ab0dbc55bfeee72b86ea3f177230
nvduc <duc.v.nguyen@prima-sol.com>
2025-10-13 14:15:15 +0700

    update

---
+++ RevitDevelop/Tools/ConvertDuctToFlex/action/IConvertDuctToFlexAction.cs
@@ -0,0 +1,12 @@
+﻿namespace RevitDevelop.Tools.ConvertDuctToFlex.action
+{
+    public interface IConvertDuctToFlexAction
+    {
+        //elements of option A
+        public void GetElementsByFittingToFitting();
+        //elements of option B
+        public void GetElementsByFittingToDistance();
+        //elements of option C
+        public void GetElementsByFittingToDistanceExactly();
+    }
+}
